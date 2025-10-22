c11ea7c177720e897e5a082211cb20ec00d87fcc
button815 <ledupgaming.calm@gmail.com>
2025-10-22 12:20:45 -0500

    Added muzzle flash

--- Assets/Scripts/Wepons.cs
+++ Assets/Scripts/Wepons.cs
@@ -1,18 +1,23 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Wepons : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public ParticleSystem muzzleflash;
+    public float fireRate;
     void Start()
     {
         
     }
-
+   
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) )
         {
+          
+            StartCoroutine(FireFlash(fireRate));
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
             {
@@ -23,6 +28,13 @@ public class Wepons : MonoBehaviour
                 }
             }
         }
-       
+        
+    }
+
+    private IEnumerator FireFlash(float duration)
+    {
+        muzzleflash.Play();
+        yield return new WaitForSeconds( duration);
+        muzzleflash.Stop();
     }
 }
